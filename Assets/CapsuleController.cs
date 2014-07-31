9ee6f41dac1e4ea55c809be2bce34a8f7ce3e490
William Riley <wrileystl@gmail.com>
2014-07-31 01:01:53 -0500

    initial commit

---
+++ Assets/CapsuleController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CapsuleController : MonoBehaviour {
+
+	private Vector3 _target;
+	public Vector3 Target
+	{
+		get { return _target; }
+		set { _target = value; }
+	}
+
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		_target = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination(_target);
+	}
+}
---
+++ Assets/ClickManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(GameObject))]
+[RequireComponent (typeof(GameObject))]
+public class ClickManager : MonoBehaviour {
+	public GameObject capsule;
+	public GameObject effectPrefab;
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Escape))
+			Application.Quit();
+
+		if (Input.GetMouseButtonDown(1))
+		{
+			RaycastHit hit;
+			if (!Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition),  out hit, 500))
+				return;
+
+			if (!hit.transform)
+				return;
+		
+			capsule.GetComponent<CapsuleController>().Target = hit.point;
+			Instantiate(effectPrefab, hit.point, Quaternion.identity);
+			//Debug.Log("Target: " + hit.point.ToString());
+		}
+	}
+}
